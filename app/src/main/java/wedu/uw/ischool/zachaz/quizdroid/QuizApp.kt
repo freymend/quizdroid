112b78b7c90d66e416aa2bf63d0c699530e1b748
freymond <37636251+freymend@users.noreply.github.com>
2023-11-04 12:15:38 -0700

    load values

--- app/src/main/java/wedu/uw/ischool/zachaz/quizdroid/QuizApp.kt
+++ app/src/main/java/wedu/uw/ischool/zachaz/quizdroid/QuizApp.kt
@@ -8,12 +8,97 @@ data class Quiz(val question: String, val answers: List<String>, val correctAnsw
 data class Topic(val title: String, val description: String, val questions: List<Quiz>)
 
 interface ITopicRepository {
-    fun load()
+    fun load(): List<Topic>
 }
 
 class TopicRepository : ITopicRepository {
-    override fun load() {
-        // TODO: Not yet implemented
+    private val topics = load()
+    override fun load(): List<Topic> {
+        val marvelQuestions = listOf(
+            Quiz(
+                R.string.marvel_question_1.toString(),
+                listOf(
+                    R.string.spiderman.toString(),
+                    R.string.batman.toString(),
+                    R.string.he_man.toString(),
+                    R.string.ninjas.toString()
+                ),
+                0
+            ),
+            Quiz(
+                R.string.marvel_question_2.toString(),
+                listOf(
+                    R.string.spiderman.toString(),
+                    R.string.batman.toString(),
+                    R.string.he_man.toString(),
+                    R.string.ninjas.toString()
+                ),
+                0
+            )
+        )
+        val marvel = Topic(
+            R.string.marvel_super_heroes.toString(),
+            R.string.marvel_description.toString(),
+            marvelQuestions
+        )
+
+        val physicsQuestions = listOf(
+            Quiz(
+                R.string.physics_question_1.toString(),
+                listOf(
+                    R.string.python.toString(),
+                    R.string.verb.toString(),
+                    R.string.bacteria.toString(),
+                    R.string.gravity.toString()
+                ),
+                3
+            ),
+            Quiz(
+                R.string.physics_question_2.toString(),
+                listOf(
+                    R.string.python.toString(),
+                    R.string.verb.toString(),
+                    R.string.bacteria.toString(),
+                    R.string.gravity.toString()
+                ),
+                3
+            )
+        )
+        val physics = Topic(
+            R.string.physics.toString(),
+            R.string.physics_description.toString(),
+            physicsQuestions
+        )
+
+        val mathQuestions = listOf(
+            Quiz(
+                R.string.math_question_1.toString(),
+                listOf(
+                    R.string.tree.toString(),
+                    R.string.algebra.toString(),
+                    R.string.earth.toString(),
+                    R.string.boom.toString()
+                ),
+                1
+            ),
+            Quiz(
+                R.string.math_question_2.toString(),
+                listOf(
+                    R.string.tree.toString(),
+                    R.string.algebra.toString(),
+                    R.string.earth.toString(),
+                    R.string.boom.toString()
+                ),
+                1
+            )
+        )
+        val math = Topic(
+            R.string.math.toString(),
+            R.string.math_description.toString(),
+            mathQuestions
+        )
+
+        return listOf(marvel, physics, math)
     }
 }
 
